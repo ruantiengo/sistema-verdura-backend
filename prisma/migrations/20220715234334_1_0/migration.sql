19d4444300ef4703c56dc0bc7cd05211d36ede4f
Ruan Tiengo <ruantiengo@gmail.com>
2022-07-15 20:51:18 -0300

    chore: add prisma

---
+++ prisma/migrations/20220715234334_1_0/migration.sql
@@ -0,0 +1,82 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "phone" VARCHAR(50) NOT NULL,
+    "type" VARCHAR(50) NOT NULL,
+    "cpf_cnpj" VARCHAR(50) NOT NULL,
+    "birthday" VARCHAR(50),
+    "note" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "number" VARCHAR(50) NOT NULL,
+    "district" VARCHAR(50) NOT NULL,
+    "city" VARCHAR(50) NOT NULL,
+    "state" VARCHAR(50) NOT NULL,
+    "clientId" INTEGER,
+    "providerId" INTEGER,
+    "employeeId" INTEGER,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Provider" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "phone" VARCHAR(50) NOT NULL,
+    "type" VARCHAR(50) NOT NULL,
+    "cpf_cnpj" VARCHAR(50) NOT NULL,
+    "note" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Provider_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "phone" VARCHAR(50) NOT NULL,
+    "monthly_salary" DOUBLE PRECISION NOT NULL,
+    "profesion" VARCHAR(50) NOT NULL,
+    "cpf" VARCHAR(50) NOT NULL,
+    "note" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_clientId_key" ON "Address"("clientId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_providerId_key" ON "Address"("providerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_employeeId_key" ON "Address"("employeeId");
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_providerId_fkey" FOREIGN KEY ("providerId") REFERENCES "Provider"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
